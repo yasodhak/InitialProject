0ba64fb91c5f5608ba73c910e38a09d3865da84a
Yasodha Krishnasamy <yasodha-k@hcl.com>
2017-07-18 17:09:54 +0530

    Simple hello program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.com.validation;
+
+public class HelloWorld {
+//Simple program	
+	 public static void main(String[] args) {
+	      
+	      System.out.println("Hello, World");
+	   }
+
+}
