623192ae9916c00a13241a7fab15db3a5cba8263
Yasodha Krishnasamy <yasodha-k@hcl.com>
2017-07-20 16:18:55 +0530

    Modified same hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,12 @@ public class HelloWorld {
 //Simple program	
 	 public static void main(String[] args) {
 	      
+		  int d=3;
+		  int e=1;
+		  int f=d-e;
+		  
 	      System.out.println("Hello, World");
+		  System.out.println("Hello, World"+f);
 	   }
 
 }
